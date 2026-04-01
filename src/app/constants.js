2f6669a39f7e64df1cb740f285674eba5c3e3eba
Vagner <santos.vagnerferreira@gmail.com>
2026-04-01 19:21:17 -0300

    chore: deletes old game files

--- src/app/constants.js
+++
@@ -1,9 +0,0 @@
-export const SPOTS_PER_COLUMNS_COUNT = 6
-export const COLUMN_COUNT = 7
-export const SPOT_RADIUS = 87 / 2
-export const SPOT_MARGIN = 8
-
-export const PLAYERS = {
-  USER: 'user',
-  MACHINE: 'machine',
-}
--- src/app/index.js
+++
@@ -1,5 +0,0 @@
-import { Game } from './Game'
-
-document.addEventListener('DOMContentLoaded', () => {
-  new Game(document.querySelector('#game-canvas')).init()
-})
